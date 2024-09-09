bdfe9e78cfd252d62ebf0de574dd7a6f6d726dc7
Patrick Williamson <upm8pb@virginia.edu>
2024-09-09 19:41:23 -0400

    uipdating

--- embed_images.py
+++ embed_images.py
@@ -1,11 +1,11 @@
-from helpers import list_files, download_files, save_progress, get_progess
+from helpers import list_files, download_files, save_progress, get_progess, load_images
 import deepface
 import os
 
 def main():
 
-    image_files = list_files('offender-images')
-    print(get_progess())
+    image_paths = load_images()
+    print(len(image_paths))
 
 if __name__ == '__main__':
     main()
