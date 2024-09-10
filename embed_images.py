8973a17a62a03e07a13acc32080d7d4a060b5ec8
Patrick Williamson <upm8pb@virginia.edu>
2024-09-10 16:59:22 -0400

    updating;

--- embed_images.py
+++ embed_images.py
@@ -1,33 +1,37 @@
 from helpers import load_images
 import os
 from multiprocessing import Pool
+import torch
+from deepface import DeepFace
+from torch.nn import DataParallel
+import math
+import numpy as np
 
-def embed_chunk_on_gpu(image_paths, gpu_id):
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
-    
-    from deepface import DeepFace
-    import numpy as np
+model = 'Facenet512'
+model = DataParallel(model)
+model.cuda()
+batch_size = 10
 
+def embed_batches(image_paths):
+    folder = '/home/patrickwilliamson/embeddings'
     for image in image_paths:
         try:
-            embedded_image = np.array(DeepFace.represent(
-                img_path=image,
-                model_name='Facenet512'
-            ))
-            output = image.split('.')[0].split('/')[-1]
-            np.save(f'/path/to/output/{output}.npy', embedded_image)
-            print(f'Embedded {image} on GPU {gpu_id}')
+            embedding = DeepFace.represent(img_path=image, model_name="Facenet512", model=model.module)
+            base_name = image.split('.')[0].split('/')[-1]
+            npy_name = f'{folder}/{base_name}.npy'
+            np.save(npy_name, np.array(embedding))
+            print(f'saved embedding {npy_name}')
         except Exception as e:
-            print(f'Error embedding image {image} on GPU {gpu_id}: {e}')
+            print(f'error with embedding {e}')
+            continue
+    print('done with batch')
 
 def main():
     image_paths = load_images('/home/patrickwilliamson/tmp/images')
-    chunk_size = 25
-    
-    chunks = [image_paths[i:i + chunk_size] for i in range(0, len(image_paths), chunk_size)]
-    
-    with Pool(processes=4) as pool:  # Assuming 4 GPUs
-        pool.starmap(embed_chunk_on_gpu, [(chunk, i % 4) for i, chunk in enumerate(chunks)])
+    chunk_size = 10
 
+    for i in range(0, 100, chunk_size):
+        embed_batches(image_paths[i:i+chunk_size]) 
+   
 if __name__ == '__main__':
     main()
