810c0cb707238ba02f659dd0ea82b51ed383cfe1
Patrick Williamson <upm8pb@virginia.edu>
2024-09-11 14:20:32 -0400

    updating

--- remove_saved.py
+++ remove_saved.py
@@ -1,8 +1,8 @@
 import glob
 import os
 def main():
-    images = glob.glob('/home/patrickwilliamson/tmp/images/*', recursive=True)
-    embedded = glob.glob('/home/patrickwilliamson/embedded/*', recursive=True)
+    images = glob.glob('/home/patrickwilliamson/tmp/images/*')
+    embedded = glob.glob('/home/patrickwilliamson/embeddings/*')
     
     images = [i.split('.')[-1].split('/')[-1] for i in images]
     embedded = [i.split('.')[-1].split('/')[-1] for i in embedded]
