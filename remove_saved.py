c3d723cd840e49f74cae390d5a98ad5d972222b0
Patrick Williamson <upm8pb@virginia.edu>
2024-09-11 14:23:14 -0400

    updating

--- remove_saved.py
+++ remove_saved.py
@@ -4,13 +4,15 @@ def main():
     images = glob.glob('/home/patrickwilliamson/tmp/images/*')
     embedded = glob.glob('/home/patrickwilliamson/embeddings/*')
     
+    ending = images[0].split('.')[-1]
     images = [i.split('.')[0].split('/')[-1] for i in images]
     print(embedded[0])
     embedded = [i.split('.')[0].split('/')[-1] for i in embedded]
-    print(images[0])
-    print(embedded[0])
     verified_e = [x for x in images if x in embedded]
     print(len(verified_e))
 
+    for file in verified_e:
+        os.remove(f'{file}.{ending}')
+        
 if __name__ == '__main__':
     main()
